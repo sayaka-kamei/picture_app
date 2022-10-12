07f4a5bbd1203d305e30ce2f6cbc022191dd8ff7
sayaka kamei <sayukou316@gmail.com>
2022-10-13 00:00:18 +0900

    プロフィール編集を作成した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path, notice: "プロフィールを編集しました"
+    else
+      render :edit
+    end    
+  end
+
   private
 
   def user_params
