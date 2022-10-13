360911597c3aa282f9c169543e4773629e795692
sayaka kamei <sayukou316@gmail.com>
2022-10-13 20:37:18 +0900

    投稿詳細画面を作成した

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,6 +13,10 @@ class PicturesController < ApplicationController
     redirect_to new_picture_path
   end
   
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
   private
 
   def picture_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/pictures', to: 'pictures#index'
-  resources :sessions,only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :pictures
 end
