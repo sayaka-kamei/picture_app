01a4d8e0fe8d2de021cd740196d5c25bf6224a5f
sayaka kamei <sayukou316@gmail.com>
2022-10-13 23:06:08 +0900

    編集画面を作成した

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,12 +9,29 @@ class PicturesController < ApplicationController
   end
   
   def create
-    Picture.create(picture_params)
-    redirect_to new_picture_path
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to new_picture_path, notice: "作成しました！"
+    else
+      render :new
+    end
   end
   
   def show
     @picture = Picture.find(params[:id])
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end  
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "編集しました！"
+    else
+      render :edit
+    end   
   end
 
   private
@@ -22,4 +39,9 @@ class PicturesController < ApplicationController
   def picture_params
     params.require(:picture).permit(:image, :content)
   end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end  
 end
+
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,3 @@
 class Picture < ApplicationRecord
+  validates :content, presence: true
 end
