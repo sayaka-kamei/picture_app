75e7203f649345cdde53718f3add3eb8789f5a25
sayaka kamei <sayukou316@gmail.com>
2022-10-17 22:44:26 +0900

    init

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,14 @@ module PictureApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
